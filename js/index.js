a17da26c2f9ae14d8fff36583daed953228c3040
miriammadava <miriammadava@gmail.com>
2021-03-26 17:32:36 -0700

    updated button functions css

--- js/index.js
+++ js/index.js
@@ -15,16 +15,3 @@ $(".navigation ").on("click", function() {
     }, 1000);
 });
 
-
-
-$(document).ready(function(){
-    $(“#hidden”).hover(function(){
-        $(this).css(“color”,“black”);
-    },
-    function(){
-        $(this).hide();
-    });
-    $(“button”).click(function(){
-        alert(“You can download each case study separately in PDF”)
-    });
-});
