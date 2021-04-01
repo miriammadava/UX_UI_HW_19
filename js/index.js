49ee3aad99674c3de857a69cf3bc56e376fc87ee
miriammadava <miriammadava@gmail.com>
2021-03-31 21:07:45 -0700

    updates js

--- js/index.js
+++ js/index.js
@@ -2,22 +2,8 @@ console.log("index.js file")
 
 
 $( ".buttonPrimary" ).click(function() {
-    $(this).css('color','red');
+    $(this).css('color','white');
   });
 
 
-$(".navigation").on("click", function() {
-    $(".inline-block").fadeOut(500);
-    $("this").delay("slow").fadeIn(500);
-});
-$(".logo").on("click", function(){
-    $("#aboutMe").fadeOut(500);
-    $(".myName").delay("slow").fadeIn(500);
-});
-$(".navigation ").on("click", function() {
-    var targetDiv = $(this).attr('href');
-    $('html, body').animate({
-        scrollTop: $("#projects").offset().top
-    }, 1000);
-});
 
